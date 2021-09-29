a71557c2bc77dd8836d2de44e42e3f2f54fed983
Shahid Ullah Baig <baigs108@gmail.com>
2021-09-29 18:58:11 +0500

    game scissor rock paper with one if

--- shahid.js
+++ shahid.js
@@ -8,39 +8,57 @@ let result1 = document.getElementById("output1");
 let result2 = document.getElementById("output2");
 const systemOptions = ["rock", "scissor", "paper"];
 
-function play(playerChoice, systemChoice) {
-  if (playerChoice === "rock") {
-    if (systemChoice === "scissor") {
-      return "Won";
-    } else if (systemChoice === "paper") {
-      return "Lose";
-    }
-    return "Tie";
-  } else if (playerChoice === "scissor") {
-    if (systemChoice === "paper") {
-      return "Won";
-    } else if (systemChoice === "rock") {
-      return "Lose";
-    }
-    return "Tie";
-  } else if (playerChoice === "paper") {
-    if (systemChoice === "rock") {
-      return "Won";
-    } else if (systemChoice === "scissor") {
-      return "Lose";
-    }
-    return "Tie";
+function play(playerChoice, computerChoice) {
+  if (
+    (playerChoice === "scissor" && computerChoice === "paper") ||
+    (playerChoice === "rock" && computerChoice === "scissor") ||
+    (playerChoice === "paper" && computerChoice == "rock")
+  ) {
+    return "you won";
+  } else if (
+    (computerChoice === "scissor" && playerChoice === "paper") ||
+    (computerChoice === "rock" && playerChoice === "scissor") ||
+    (computerChoice === "paper" && playerChoice == "rock")
+  )
+    return "you lost";
+  else {
+    return "tie";
   }
-  return "Over";
 }
+
+// function play(playerChoice, systemChoice) {
+//   if (playerChoice === "rock") {
+//     if (systemChoice === "scissor") {
+//       return "Won";
+//     } else if (systemChoice === "paper") {
+//       return "Lose";
+//     }
+//     return "Tie";
+//   } else if (playerChoice === "scissor") {
+//     if (systemChoice === "paper") {
+//       return "Won";
+//     } else if (systemChoice === "rock") {
+//       return "Lose";
+//     }
+//     return "Tie";
+//   } else if (playerChoice === "paper") {
+//     if (systemChoice === "rock") {
+//       return "Won";
+//     } else if (systemChoice === "scissor") {
+//       return "Lose";
+//     }
+//     return "Tie";
+//   }
+//   return "Over";
+// }
 
 btn_play.addEventListener("click", function () {
   let player_choice = options.value;
   //   console.log(player_choice);
   const choiceNumber = Math.floor(Math.random() * 3);
-  const computerChoice = systemOptions[choiceNumber];
-  let game_result = play(player_choice, computerChoice);
-  result1.innerHTML = computerChoice;
+  const computerChoice1 = systemOptions[choiceNumber];
+  let game_result = play(player_choice, computerChoice1);
+  result1.innerHTML = computerChoice1;
   result2.innerHTML = player_choice;
   result.innerHTML = game_result;
 });
